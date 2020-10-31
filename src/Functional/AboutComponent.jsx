d97fe8cbc2ba29621023e394d6814f07e6ef9147
magdmagd <dv.aboelmagd@gmail.com>
2020-11-01 01:09:59 +0200

    Update Finish Assignment2

--- src/Functional/AboutComponent.jsx
+++ src/Functional/AboutComponent.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {
+    const leaders = props.leaders.map((leader) => 
+    {
         return (
-            <p>Leader {leader.name}</p>
+           // <p>Leader {leader.name}</p>
+           <RenderLeader leader={leader} />
         );
     });
+
+
+    function RenderLeader({ leader }) 
+    {
+        return (
+            
+                <Media tag="li" key={leader.id} className="col-12 mt-5" >
+                    <Media left middle>
+                        <Media object src={leader.image} alt={leader.name} />
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>{leader.name}</Media>
+                        <p>{leader.designation}</p>
+                        <p>{leader.description}</p>
+                    </Media>
+                </Media>
+        
+        );
+    }//end function Renderleader
+
+    function RenderLeaders() 
+    {
+
+
+     return (
+            <Media list>
+                <Stagger in>
+                    {leaders}
+                </Stagger>
+            </Media>
+        );
+    }//end RenderLeaders() 
+
 
     return(
         <div className="container">
@@ -65,13 +101,17 @@ function About(props) {
                     <h2>Corporate Leadership</h2>
                 </div>
                 <div className="col-12">
-                    <Media list>
-                        {leaders}
-                    </Media>
+                    <RenderLeaders/>
                 </div>
             </div>
         </div>
     );
 }
+/**
+ * <Media list>
+     {leaders}
+    </Media>
+
+ */
 
 export default About;    
