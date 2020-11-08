e4fc77723439e05188463d56795d97de92e80a09
magdmagd <dv.aboelmagd@gmail.com>
2020-11-08 13:41:48 +0200

    Redux : Split Store to many

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,33 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+//import {createStore} from 'redux';
+//import { Reducer, initialState } from './reducer'
+import {createStore, combineReducers} from 'redux';
+import { Dishes } from './dishes';
+import { Dishes2 } from './dishes2';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore = () => 
 {
     const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
+         combineReducers({
+            dishes: Dishes,
+            dishes2: Dishes2,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
 }//end ConfigureStore
+
+
+/**
+ * Update Many Stores
+ * const store = createStore(
+        Reducer, // reducer
+        initialState, // our initialState
+    );
+ * 
+ */
---
+++ src/redux/dishes2.js
@@ -0,0 +1,10 @@
+import { DISHES2 } from '../shared/dishes2';
+
+export const Dishes2 = (state = DISHES2, action) => 
+    {
+    switch (action.type)
+     {
+        default:
+          return state;
+    }//end switch
+};
